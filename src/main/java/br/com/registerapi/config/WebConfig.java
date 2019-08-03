1eea3795913a9e3a6a989c4d436f54e5e5940e81
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-03 02:39:36 -0300

    Inclusao de classe para liberar cors browser

---
+++ src/main/java/br/com/registerapi/config/WebConfig.java
@@ -0,0 +1,21 @@
+package br.com.registerapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+}
