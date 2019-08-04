c0139a3a2f849d5c41cd3a0fc077e978c582a744
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-04 01:46:35 -0300

    Retirado imports nao utilizados

--- src/main/java/br/com/registerapi/config/SwaggerConfig.java
+++ src/main/java/br/com/registerapi/config/SwaggerConfig.java
@@ -1,24 +1,16 @@
 package br.com.registerapi.config;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.collect.Lists;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
