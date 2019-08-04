03f07bbfb1a58ad714af1d3c4048ced9524079ab
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-04 03:55:19 -0300

    Entity com atributos completos

--- src/main/java/br/com/registerapi/entity/CustomerEntity.java
+++ src/main/java/br/com/registerapi/entity/CustomerEntity.java
@@ -1,5 +1,7 @@
 package br.com.registerapi.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(schema = "customers", name = "customers",
@@ -23,4 +27,15 @@ public class CustomerEntity {
     @GeneratedValue(generator = "customerGenerator")
     @SequenceGenerator(name = "customerGenerator", schema = "customers", sequenceName = "customer_seq", allocationSize = 1)
     public Long id;
+    
+    @Column(name = "name")
+    public String name;
+    
+    @Column(name = "age")
+    public Integer age;
+    
+    @CreationTimestamp
+    @Column(name = "created_at")
+    public Timestamp createAt;
+
 }
