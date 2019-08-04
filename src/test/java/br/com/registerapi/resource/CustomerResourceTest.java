d3a271a01a007f8ae3fb5db8e6a78d218a38348e
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-04 03:09:16 -0300

    Inclusao classe de teste de controllers

---
+++ src/test/java/br/com/registerapi/resource/CustomerResourceTest.java
@@ -0,0 +1,44 @@
+package br.com.registerapi.resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CustomerResourceTest {
+	
+	@InjectMocks
+	private CustomerResource customerResource;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testCreateCustomer() {
+		
+	}
+	
+	@Test
+	public void testUpdateCustomer() {
+		
+	}
+	
+	@Test
+	public void testGetCustomerById() {
+		
+	}
+	
+	@Test
+	public void testGetAllCustomers() {
+		
+	}
+	
+	@Test
+	public void testDeleteCustomerById() {
+		
+	}
+}
