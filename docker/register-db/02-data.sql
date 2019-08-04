128b872df2b4fd12542991e842b89d5e834eee87
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-04 01:57:26 -0300

    Refactor nos inserts onde houve mudanca em nome de tabela

--- docker/register-db/02-data.sql
+++ docker/register-db/02-data.sql
@@ -16,30 +16,30 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: clients; Type: TABLE DATA; Schema: clients; Owner: root
+-- Data for Name: customers; Type: TABLE DATA; Schema: customers; Owner: root
 --
 
-INSERT INTO clients.clients (id, name, age, created_at) values (nextval('clients.clients_seq'), 'Joaquim Floriano', 35, '2019-08-02 01:29:00.575000');
+INSERT INTO customers.customers (id, name, age, created_at) values (nextval('customers.customers_seq'), 'Joaquim Floriano', 35, '2019-08-02 01:29:00.575000');
 
 --
--- Data for Name: geolocation; Type: TABLE DATA; Schema: clients; Owner: root
+-- Data for Name: geolocation; Type: TABLE DATA; Schema: customers; Owner: root
 --
 
-INSERT INTO clients.geolocation (id, client_id, ipv4, continent_name, country_name, subdivision_1_name, subdivision_2_name, city_name, latitude, longitude, created_at)     values (nextval('clients.geolocation_seq'), 1, '8.8.8.8', 'North America', 'United States', 'California', null, 'Mountain View', '37.38600', '-122.08380', '2019-08-02 01:29:00.575000');
+INSERT INTO customers.geolocation (id, customer_id, ipv4, continent_name, country_name, subdivision_1_name, subdivision_2_name, city_name, latitude, longitude, created_at)     values (nextval('customers.geolocation_seq'), 1, '8.8.8.8', 'North America', 'United States', 'California', null, 'Mountain View', '37.38600', '-122.08380', '2019-08-02 01:29:00.575000');
 
 --
--- Data for Name: woeid; Type: TABLE DATA; Schema: clients; Owner: root
+-- Data for Name: woeid; Type: TABLE DATA; Schema: customers; Owner: root
 --
 
-INSERT INTO clients.woeid (id, client_id, distance, title, location_type, woeid, latt_long, created_at) 
-    values (nextval('clients.woeid_seq'), 1, 1599, 'Mountain View', 'City', 2455920, '37.39999,-122.079552', '2019-08-02 01:29:00.575000');
+INSERT INTO customers.woeid (id, customer_id, distance, title, location_type, woeid, latt_long, created_at) 
+    values (nextval('customers.woeid_seq'), 1, 1599, 'Mountain View', 'City', 2455920, '37.39999,-122.079552', '2019-08-02 01:29:00.575000');
 
 --
--- Data for Name: weather; Type: TABLE DATA; Schema: clients; Owner: root
+-- Data for Name: weather; Type: TABLE DATA; Schema: customers; Owner: root
 --
 
-INSERT INTO clients.weather (id, client_id, max_temp, min_temp, applicable_date, created_at) 
-    values (nextval('clients.weather_seq'), 1, 26.14, 16.055, '2019-08-02', '2019-08-02 01:29:00.575000');
+INSERT INTO customers.weather (id, customer_id, max_temp, min_temp, applicable_date, created_at) 
+    values (nextval('customers.weather_seq'), 1, 26.14, 16.055, '2019-08-02', '2019-08-02 01:29:00.575000');
 
 --
 -- PostgreSQL database dump complete
