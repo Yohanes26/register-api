38a757465ad6f5d9ced258629272affecfeadb4a
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-04 03:34:48 -0300

    Annotation de repository e inclusao de entidade no repository

--- src/main/java/br/com/registerapi/repository/CustomerRepository.java
+++ src/main/java/br/com/registerapi/repository/CustomerRepository.java
@@ -1,5 +1,12 @@
 package br.com.registerapi.repository;
 
-public class CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.registerapi.entity.CustomerEntity;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long>{
 
 }
