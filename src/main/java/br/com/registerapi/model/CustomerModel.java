72abb14cdb070b2ef18efd7cd5d38025d12abba1
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-04 04:04:59 -0300

    Validacoes em annotation

--- src/main/java/br/com/registerapi/model/CustomerModel.java
+++ src/main/java/br/com/registerapi/model/CustomerModel.java
@@ -1,5 +1,48 @@
 package br.com.registerapi.model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CustomerModel {
 
+	@JsonIgnore
+	@JsonProperty("id")
+	private Long id;
+    
+	@NotNull
+	@Size(max = 255)
+    @JsonProperty("name")
+    private String name;
+    
+	@NotNull
+	@Size(max = 3)
+    @JsonProperty("age")
+	private Integer age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 }
