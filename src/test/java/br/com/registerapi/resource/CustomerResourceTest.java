81fb3eea30fe40e6a8840a278809c87f35b7329a
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-05 01:25:22 -0300

    Retirado imports nao utilizados

--- src/test/java/br/com/registerapi/resource/CustomerResourceTest.java
+++ src/test/java/br/com/registerapi/resource/CustomerResourceTest.java
@@ -1,13 +1,10 @@
 package br.com.registerapi.resource;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import java.util.ArrayList;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -17,11 +14,9 @@ import org.mockito.InjectMocks;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 import br.com.registerapi.model.CustomerModel;
