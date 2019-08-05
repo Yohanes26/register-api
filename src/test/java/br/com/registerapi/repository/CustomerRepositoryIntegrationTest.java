f013925d64e8b1da7ab92143b40a789cfc36e265
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-08-05 01:32:00 -0300

    TDD - Inclusao de classe de teste de integracao do repository

---
+++ src/test/java/br/com/registerapi/repository/CustomerRepositoryIntegrationTest.java
@@ -0,0 +1,19 @@
+package br.com.registerapi.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CustomerRepositoryIntegrationTest {
+ 
+    @Autowired
+    private TestEntityManager entityManager;
+ 
+    @Autowired
+    private CustomerRepository customerRepository;
+ 
+}
